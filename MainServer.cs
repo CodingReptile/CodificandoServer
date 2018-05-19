e0b88f0f4ceca279958ac6bbfc71ce77d672ed76
Juan Ochoa <juancamiloochoa@gmail.com>
2018-05-18 19:42:45 -0700

    First basic version

---
+++ MainServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodificandoServer
+{
+    public class MainServer
+    {
+        public static void Main(string[] args)
+        {
+            var handler = new RequestHandler();
+
+            try
+            {
+                handler.Initialize();
+                handler.StartMainLoop();
+            }
+            catch (Exception e)
+            {
+                PrintError(e);
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private static void PrintError(object obj)
+        {
+            ConsoleColor current = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(obj);
+            Console.ForegroundColor = current;
+        }
+    }
+}
---
+++ RequestHandler.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodificandoServer
+{
+    // State object for reading client data asynchronously  
+    public class StateObject
+    {
+        // Client  socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 1024;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public class RequestHandler
+    {
+        private static readonly int MaxConnections = 10;
+        private static readonly int Port = 8888;
+
+
+        private Socket mainSocket;
+
+        public void Initialize()
+        {
+            this.StartListening();
+        }
+
+        public void StartMainLoop()
+        {
+            while (true)
+            {
+                mainSocket.BeginAccept(new AsyncCallback(this.AcceptCallback), mainSocket);
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Socket listener = (Socket) ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+        }
+
+        public void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read   
+                // more data.  
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    // All the data has been read from the   
+                    // client. Display it on the console.  
+                    PrintInfo("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    // Echo the data back to the client.  
+                    Send(handler, content);
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                PrintInfo("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void StartListening()
+        {
+            mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            mainSocket.Bind(new IPEndPoint(IPAddress.Loopback, RequestHandler.Port));
+            mainSocket.Listen(RequestHandler.MaxConnections);
+
+            PrintInfo(string.Format("Started listening on port {0}", RequestHandler.Port));
+        }
+
+        private static void PrintInfo(string message, params object[] list)
+        {
+            Console.WriteLine(message, list);
+        }
+
+
+    }
+}
